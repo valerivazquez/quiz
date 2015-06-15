7e38fdb91f062808efd234510973b6c08c3cc427
Valeri Vazquez <valerivazquez@gmail.com>
2015-06-15 22:11:05 +0200

    Paso 9: Lista de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,20 +2,32 @@ var models = require('../models/models.js');
 
 
 // GET /quizzes/question
-exports.question = function (req, res) {
- 	models.Quiz.findAll().success(function(quiz){
- 		res.render('quizes/question', {pregunta: quiz[0].pregunta})
+exports.index = function (req, res) {
+ 	models.Quiz.findAll().then(function(quizes){
+ 		res.render('quizes/index', {quizes : quizes});
+ 	})
+	
+};
+
+
+
+// GET /quizzes/question
+exports.show = function (req, res) {
+ 	models.Quiz.find(req.params.quizId).then(function(quiz){
+ 		res.render('quizes/show', {quiz : quiz});
  	})
 	
 };
 
 // GET /quizzes/answer
 exports.answer = function (req, res) {
- 	models.Quiz.findAll().success(function(quiz){
-		if (req.query.respuesta === quiz[0].respuesta){
-			res.render('quizes/answer', {respuesta: 'Correcto'});
+ 	models.Quiz.find(req.params.quizId).then(function(quiz){
+		if (req.query.respuesta === quiz.respuesta){
+			res.render('quizes/answer', 
+				{ quiz : quiz, respuesta: 'Correcto'});
 		} else{
-			res.render('quizes/answer', {respuesta: 'Incorrecto'});
+			res.render('quizes/answer', 
+				{ quiz : quiz ,respuesta: 'Incorrecto'});
 		}
 	})
 };
--- models/models.js
+++ models/models.js
@@ -38,6 +38,9 @@ sequalize.sync().success(function() {
 		if(count ===0) {  // la tabla se inicializa solo si esta vacia
 			Quiz.create({ pregunta : 'Capital de Italia',
 						  respuesta : 'Roma'
+						});
+			Quiz.create({ pregunta : 'Capital de Portugal',
+						  respuesta : 'Lisboa'
 						})
 			.success(function(){console.log("Base de datos inicializada")});
         };
