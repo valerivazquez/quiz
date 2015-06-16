26bb7c4fbeb2ede9c5a231ccf2ead99d7f2bae62
Valeri Vazquez <valerivazquez@gmail.com>
2015-06-16 09:00:44 +0200

    Paso 10: Autoload

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,11 @@ quizController = require ('../controllers/quiz_controller');
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);  // autoload :quizId
+
+
 router.get('/quizes',					   quizController.index);
 router.get('/quizes/:quizId(\\d+)',		   quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
