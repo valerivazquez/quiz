9c7972e36a3309ccf8db0cd5c065badba098f61e
Valeri Vazquez <valerivazquez@gmail.com>
2015-06-29 19:14:26 +0200

    Paso 18: Moderacion de comentarios

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,8 @@ router.get('/', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
+router.param('commentId', commentController.load);  // autoload :commentId
+
 
 // Definición de rutas de sesion
 router.get('/login',  sessionController.new); // formulario de login
@@ -34,6 +36,8 @@ router.delete('/quizes/:quizId(\\d+)',	   sessionController.loginRequired, quizC
 // Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new',commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',   commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',
+										   sessionController.loginRequired, commentController.publish);
 
 
 router.get('/author', function(req, res) {
